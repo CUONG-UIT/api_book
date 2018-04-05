23573909c353680feaaecdef4801a85a4d67bc4e
CUONGUIT <14520114@gm.uit.edu.vn>
2018-04-05 08:47:52 +0700

    No commit

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::BooksController < ApplicationController
+  skip_before_action :verify_authenticity_token ,raise: false
+  before_action :find_book,only: [:show,:updatea,:destroy]
+
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
+  def show
+    render json: @book
+  end
+
+  def create
+    @book = current_user.books.built(book_params)
+    if @book.save
+      render json: @book , status: :ok
+    else
+      render json: @book.errors , status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      render json: @book,status: :ok
+    else
+      render json: @book.error,status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book.destroy
+    head(:ok)
+  end
+
+  private
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title,:author,:description)
+  end
+end
